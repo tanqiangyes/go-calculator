2baa9b180a69e859d5e8a4e328215dba85339748
tanqiang <tanqiang@enjoypartytime.com>
2023-11-13 18:09:20 +0800

    init

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/widget"
+	cal "github.com/mnogu/go-calculator"
+)
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Calculator")
+	myWindow.Resize(fyne.NewSize(800, 600))
+
+	input := widget.NewEntry()
+	input.SetPlaceHolder("Enter the equation")
+
+	str := binding.NewString()
+	content := container.NewVBox(input, widget.NewButton("Calculate", func() {
+		go func() {
+			f, err := calculator(input.Text)
+			log.Printf("Calculate result: %v", f)
+			if err != nil {
+				str.Set("服务器开小差了...")
+			} else {
+				str.Set(fmt.Sprint(f))
+			}
+		}()
+	}))
+	myWindow.SetContent(content)
+	text := widget.NewLabelWithData(str)
+	myWindow.SetContent(text)
+	myWindow.ShowAndRun()
+}
+
+func calculator(str string) (float64, error) {
+	return cal.Calculate(str)
+}
